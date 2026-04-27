475bab68983eaff9d2944fe8b36970cb3a8a7269
dunversatile85 <donaldtmitchell777@gmail.com>
2026-04-27 13:09:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 export default function Home() {
- return (
-   <div style={{
-     background:"#0f0f10",
-     color:"white",
-     minHeight:"100vh",
-     display:"flex",
-     justifyContent:"center",
-     alignItems:"center"
-   }}>
-     <h1>Don's Play World</h1>
-   </div>
- )
+  return (
+    <div>
+      <h1>Don's Play World</h1>
+    </div>
+  );
 }
