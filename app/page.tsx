910a1a4f27f3f33fe00c1429043f515121105d19
dunversatile85 <donaldtmitchell777@gmail.com>
2026-04-27 06:31:54 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+ return (
+   <div style={{
+     background:"#0f0f10",
+     color:"white",
+     minHeight:"100vh",
+     display:"flex",
+     justifyContent:"center",
+     alignItems:"center"
+   }}>
+     <h1>Don's Play World</h1>
+   </div>
+ )
+}
